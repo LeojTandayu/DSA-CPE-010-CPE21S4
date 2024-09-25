4ed441140a124e55181a01fe43e11d2be713a0da
Leoj Jeam B. Tandayu <qljbtandayu@tip.edu.ph>
2024-09-25 14:57:52 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,40 @@
 #include <iostream>
-#include <string>
-#include <cmath>
+#include <queue>
 using namespace std;
 
-int main() {
 
- 
-  ///1. Create a C++ program to swap the two numbers in different variables. 
-
-  int a = 10;
-  int b = 20;
-  int c;
-  c = a;
-  a = b;
-  b = c;
+void display (queue<string>pl)
+{
+    while(true)    
+    {
+        if(!pl.empty())
+        {
+        cout<<pl.front()<<endl;
+        pl.pop();
+        }
+        
+        else
+        {
+            cout<<endl;
+            cout<<"Stack is empty"<<endl;
+            break;
+        }
+    }
+    
+    
+}
   
-  cout << "1) a = " << a << endl;
+int main()
+{
+    queue <string>pl;
+    cout<<"_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"<<endl;
+    cout<<"Push elements into stack:"<<endl;
+    
+    pl.push("C++");
+    pl.push("Java");
+    pl.push("Python");
+    display(pl);
+    
 
-
-  cout<<"---------------------"<<endl;
-/*2. Create a C++ program that has a function to convert temperature in Kelvin to Fahrenheit. */
-  int kelvin;
-  
-  cout<<"2)Enter your Kelvin Temperature:";
-  cin>>kelvin;
-  int fahrenheit;
-  fahrenheit = 9/5 * (kelvin - 273) + 32;
-  cout<< "The converted temperatue is: "<<fahrenheit<<endl;
-
-  
-  cout<<"---------------------"<<endl; 
-  /*3. Create a C++ program that has a function that will calculate the distance between two points.*/
-  int x1, x2, y1, y2; 
-
-  cout<<"3)Enter your first x coordinate:";
-  cin>>x1;
-  cout<<"Enter your first y coordinate:";
-  cin>>y1;
-  cout<<"Enter your second x coordinate:";
-  cin>>x2;
-  cout<<"Enter your second y coordinate:";
-  cin>>y2;
-
-  int distance;
-  distance = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-  cout<<"The distance is:"<<distance<<endl;
-  cout<<"---------------------"<<endl; 
-
-
-  
+    return 0;
 }
